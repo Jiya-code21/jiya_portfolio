25f3899d3c02c3510655518a38c69ee2914c03e8
Jiya Agrawal <jiyaagrawal6395@gmail.com>
2026-07-01 17:54:37 +0530

    Fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
-  // 👇 Ye line add karo
-  base: "/jiya_portfolio/",
 
   resolve: {
     alias: {
